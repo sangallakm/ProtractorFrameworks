719a9691301bd41b1ae12d605e5e88ff65c79682
sangallakm <sangallakm@gmai.com>
2022-03-05 21:25:44 +0000

    changed calculator pages . sleep time increased

--- tests/calculator pages.js
+++ tests/calculator pages.js
@@ -34,6 +34,6 @@ describe('demo calculator tests', function(){
         //expect(result.getText()).toEqual('5');
         homepage.verifyResult('7');
         
-        browser.sleep(2000);
+        browser.sleep(3000);
     });
 });
